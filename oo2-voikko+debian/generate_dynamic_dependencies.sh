56e858ff88f2a15cb59b40432b21791a26f3db0e
Teemu Likonen <tlikonen@iki.fi>
2006-08-17 15:41:50 +0000

    Add Oo2-voikko's debian directory to repository
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk/debian@361 ee56993d-3414-0410-8ed4-8136e77a0dd8

---
+++ oo2-voikko+debian/generate_dynamic_dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This script generates "Depends" and "Conflicts" fields to binary package's
+# control file.
+
+substvars=debian/openoffice.org-voikko.substvars
+
+current_version=$(dpkg-query --showformat='${Version}\n' --show openoffice.org-dev)
+current_upstream_version=$(echo $current_version | sed -e 's/-[^-]*$//')
+
+dpkg-shlibdeps -O -e'build/pkg/Linux_x86/oo2-voikko.so' >$substvars 2>/dev/null
+cat <<EOF >>$substvars
+misc:Depends=openoffice.org-core (>= $current_version)
+misc:Conflicts=openoffice.org-core (>= $current_upstream_version.1)
+EOF
