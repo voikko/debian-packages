8407907e1209e55ed898480755ccb6b4a32c9db8
Teemu Likonen <tlikonen@iki.fi>
2006-09-30 09:14:51 +0000

    Ignore prerelease versions (~) when generating the "Conflicts" field against future upstream versions.
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk/debian@557 ee56993d-3414-0410-8ed4-8136e77a0dd8

--- oo2-voikko+debian/generate_dependencies.sh
+++ oo2-voikko+debian/generate_dependencies.sh
@@ -5,7 +5,7 @@
 substvars=debian/openoffice.org-voikko.substvars
 
 current_version=$(dpkg-query --showformat='${Version}\n' --show openoffice.org-dev)
-current_upstream_version=$(echo $current_version | sed -e 's/-[^-]*$//')
+current_upstream_version=$(echo $current_version | sed -e 's/-[^-]*$//;s/~[^~]*$//')
 
 dpkg-shlibdeps -O build/pkg/Linux_x86/*.so >$substvars 2>/dev/null
 cat <<EOF >>$substvars
