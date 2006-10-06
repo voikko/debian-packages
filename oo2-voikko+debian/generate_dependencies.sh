ba7f275e54863aa2ea543fdee1353d73bda6b8f9
Harri Pitkänen <hatapitk@iki.fi>
2006-10-06 14:08:58 +0000

    * Update libvoikko test packages * Fix dependency generation for openoffice.org-voikko on   non-i386 platforms
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk/debian@596 ee56993d-3414-0410-8ed4-8136e77a0dd8

--- oo2-voikko+debian/generate_dependencies.sh
+++ oo2-voikko+debian/generate_dependencies.sh
@@ -7,7 +7,7 @@ substvars=debian/openoffice.org-voikko.substvars
 current_version=$(dpkg-query --showformat='${Version}\n' --show openoffice.org-dev)
 current_upstream_version=$(echo $current_version | sed -e 's/-[^-]*$//;s/~[^~]*$//')
 
-dpkg-shlibdeps -O build/pkg/Linux_x86/*.so >$substvars 2>/dev/null
+dpkg-shlibdeps -O build/pkg/Linux_*/*.so >$substvars 2>/dev/null
 cat <<EOF >>$substvars
 misc:Depends=openoffice.org-core (>= $current_version)
 misc:Conflicts=openoffice.org-core (>= $current_upstream_version.1)
