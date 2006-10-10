cad208d93fde3af1a6db785a69e96dc5ceebbb18
Teemu Likonen <tlikonen@iki.fi>
2006-10-10 09:59:50 +0000

    * New SVN snapshot * Component is now named openoffice.org-voikko * Add Recommends field
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk/debian@614 ee56993d-3414-0410-8ed4-8136e77a0dd8

--- openoffice.org-voikko+debian/generate_dependencies.sh
+++ openoffice.org-voikko+debian/generate_dependencies.sh
@@ -7,6 +7,9 @@ substvars=debian/openoffice.org-voikko.substvars
 current_version=$(dpkg-query --showformat='${Version}\n' --show openoffice.org-dev)
 current_upstream_version=$(echo $current_version | sed -e 's/-[^-]*$//;s/~[^~]*$//')
 
+#LD_LIBRARY_PATH="/usr/lib/openoffice/program" dpkg-shlibdeps -O build/pkg/Linux_*/*.so >$substvars 2>/dev/null
+# In Ubuntu Dapper LD_LIBRARY_PATH does not help us to get correct dependencies
+# for openoffice.org-core. We must insert it manually.
 dpkg-shlibdeps -O build/pkg/Linux_*/*.so >$substvars 2>/dev/null
 cat <<EOF >>$substvars
 misc:Depends=openoffice.org-core (>= $current_version)
