83cccd57664b2ae0875ba301a3e4a56acf608c29
Teemu Likonen <tlikonen@iki.fi>
2006-10-27 06:25:18 +0000

    Replace command substitution and sed script with shell's parameter expansion.
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk/debian@691 ee56993d-3414-0410-8ed4-8136e77a0dd8

--- openoffice.org-voikko+debian/generate_dependencies.sh
+++ openoffice.org-voikko+debian/generate_dependencies.sh
@@ -5,13 +5,14 @@
 substvars=debian/openoffice.org-voikko.substvars
 
 current_version=$(dpkg-query --showformat='${Version}\n' --show openoffice.org-dev)
-current_upstream_version=$(echo $current_version | sed -e 's/-[^-]*$//;s/~[^~]*$//')
+depends=${current_version%-*}
+conflicts=${depends%~*}.1
 
 #LD_LIBRARY_PATH="/usr/lib/openoffice/program" dpkg-shlibdeps -O build/pkg/Linux_*/*.so >$substvars 2>/dev/null
 # In Ubuntu Dapper LD_LIBRARY_PATH does not help us to get correct dependencies
 # for openoffice.org-core. We must insert it manually.
 dpkg-shlibdeps -O build/pkg/Linux_*/*.so >$substvars 2>/dev/null
 cat <<EOF >>$substvars
-misc:Depends=openoffice.org-core (>= $current_version)
-misc:Conflicts=openoffice.org-core (>= $current_upstream_version.1)
+misc:Depends=openoffice.org-core (>= $depends)
+misc:Conflicts=openoffice.org-core (>= $conflicts)
 EOF
