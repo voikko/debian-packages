3b1466ea6bb70f88a4e718dd8278f76df32221bf
Teemu Likonen <tlikonen@iki.fi>
2006-12-10 14:21:08 +0000

    Remove ".dfsg" from version number when generating Conflicts fields against future releases.
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk/debian@850 ee56993d-3414-0410-8ed4-8136e77a0dd8

--- openoffice.org-voikko+debian/generate_dependencies.sh
+++ openoffice.org-voikko+debian/generate_dependencies.sh
@@ -6,7 +6,8 @@ substvars=debian/openoffice.org-voikko.substvars
 
 current_version=$(dpkg-query --showformat='${Version}\n' --show openoffice.org-dev)
 depends=${current_version%-*}
-conflicts=${depends%~*}.1
+conflicts=${depends%%.dfsg*}
+conflicts=${conflicts%~*}.1
 
 #LD_LIBRARY_PATH="/usr/lib/openoffice/program" dpkg-shlibdeps -O build/pkg/Linux_*/*.so >$substvars 2>/dev/null
 # In Ubuntu Dapper LD_LIBRARY_PATH does not help us to get correct dependencies
